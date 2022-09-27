44d1e5315f505b66ad97fe480cee0969de4e0755
Mary <mcrowe0331@gmail.com>
2022-09-27 17:35:36 -0500

    Adding remaining stub routes

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const app = express()
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.send('Hello world!!')
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page<h1>')
+    res.status(404).send('<h1> 404 Page <h1>')
 })
 
 app.listen(process.env.PORT)
