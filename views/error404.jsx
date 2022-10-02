c33aff58fd25473f8141029dfad6cabe62f4a049
Mary <mcrowe0331@gmail.com>
2022-10-02 13:10:29 -0500

    Adding 404 page view

---
+++ views/error404.jsx
@@ -0,0 +1,13 @@
+const React = require('react')
+const Def = require('./default')
+
+function error404 () {
+    return (
+        <Def>
+            <main>
+                <h1>404: PAGE NOT FOUND</h1>
+            </main>
+        </Def>
+    )
+}
+module.exports = error404
