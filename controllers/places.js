a7a6614754943b50664ba543a52f73541c9d1e16
Mary Elizabeth <maryelizabeth@Marys-MacBook-Pro.local>
2022-10-09 12:24:40 -0500

    updates

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const places= require('../models/places.js')
 
 router.get('/new', (req, res) => {
   res.render('places/new')
@@ -14,8 +15,20 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
   console.log(req.body)
-  res.send('POST /places')
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
+
 
 
 router.get('/', (req, res) => {
