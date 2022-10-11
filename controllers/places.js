39ff2bdb23f851667c6b578af7c3a2f6449d3423
Mary Elizabeth <maryelizabeth@Marys-MacBook-Pro.local>
2022-10-10 23:38:49 -0400

    Delete functionality implemeted

--- controllers/places.js
+++ controllers/places.js
@@ -4,8 +4,7 @@ const places= require('../models/places.js')
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
-router.get('/', (req, res) => {
-             
+router.get('/', (req, res) => {         
   res.render('places/index', {places})
 })
 router.get('/:id', (req, res) => {
@@ -16,7 +15,7 @@ router.get('/:id', (req, res) => {
   else if (!places[id]) {
   }
     else {
-  res.render('/places/show', { place: places[id] })
+  res.render('/places/show', { place: places[id], id })
   }
 }) 
 
@@ -36,6 +35,21 @@ router.post('/', (req, res) => {
   places.push(req.body)
   res.redirect('/places')
 })
+
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
+})
+
 
 
 
