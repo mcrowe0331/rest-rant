d83aca2886ed85e6b35c3a22e5f85de252fa0af9
Mary Elizabeth <maryelizabeth@Marys-MacBook-Pro.local>
2022-10-10 22:49:52 -0400

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -8,9 +8,17 @@ router.get('/', (req, res) => {
              
   res.render('places/index', {places})
 })
-// router.get('/:id', (req, res) => {
-//   res.render('/places/id')
-// }) 
+router.get('/:id', (req, res) => {
+  let id= Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+  }
+    else {
+  res.render('/places/show', { place: places[id] })
+  }
+}) 
 
 
 router.post('/', (req, res) => {
