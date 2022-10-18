f3c10c57d36e3edb8edb4494e37f1204c8289adc
Mary Elizabeth <maryelizabeth@Marys-MacBook-Pro.local>
2022-10-18 16:45:06 -0500

    calculate place ratings

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,10 +7,24 @@ function show (data) {
       No comments yet!
     </h3>
   )
+  let rating = (
+    <h3 className="inactive">
+      Not yet rated
+    </h3>
+  )
   if (data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars
+    }, 0)
+    let averageRating = sumRatings / data.place.comments.length
+    rating = (
+      <h3>
+        {averageRating} stars
+      </h3>
+  )
     comments = data.place.comments.map(c => {
       return (
-        <div className="border">
+        <div className="border col-sm-4">
           <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
           <h4>{c.content}</h4>
           <h3>
